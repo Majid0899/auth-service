bd47aaaf2c2f338c871d929c52cc24813d1b34de
Majid0899 <majidkhan0899@gmail.com>
2025-09-24 17:58:29 +0530

    feat(utils): add jwt  generate token and verify token

---
+++ utils/jwt.ts
@@ -0,0 +1,42 @@
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+
+const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "refreshsecret";
+
+
+interface payload {
+    id:number,
+    name:string,
+    role:"user" | "admin"
+}
+
+const generateToken=(user:payload):{accessToken:string,refreshToken:string}=>{
+
+    const accessToken=jwt.sign({id:user.id,name:user.name,role:user.role},JWT_SECRET, { expiresIn: "30m" });
+
+    const refreshToken = jwt.sign({ id: user.id,name:user.name,role: user.role },JWT_REFRESH_SECRET,{expiresIn: "7d" });
+
+    return {accessToken,refreshToken}
+}
+
+const verifyaccessToken=(token:string):payload=>{
+    const decoded=jwt.verify(token,JWT_SECRET) as JwtPayload
+    return {
+        id:decoded.id as number,
+        name:decoded.name as string,
+        role:decoded.role as "user" | "admin",
+    }
+}
+
+const verifyrefreshToken=(token:string):payload=>{
+    const decoded=jwt.verify(token,JWT_SECRET) as JwtPayload
+    return {
+        id:decoded.id as number,
+        name:decoded.name as string,
+        role:decoded.role as "user" | "admin",
+    }
+}
+
+export {generateToken,verifyaccessToken,verifyrefreshToken,payload}
+
