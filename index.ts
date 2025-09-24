cf4526d92160e4aa0513028985ff400cb35034a6
Majid0899 <majidkhan0899@gmail.com>
2025-09-24 18:00:14 +0530

    feat: add user router in entry point

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+
+import {connectDB} from "./config/db.js"
+
+import userRouter from "./routes/userRoute.ts"
+
 dotenv.config();
 
+connectDB()
 const app: Application = express();
 const PORT: number = Number(process.env.PORT) || 3000;
 const URL: string = process.env.URL || 'http://localhost';
 
 
-
+app.use(express.json());
 app.get('/', (req: Request, res: Response):void => {
   res.send('Welcome to Auth Service');
 });
 
-
+app.use("/user",userRouter)
 
 // Start server
 app.listen(PORT, (): void => {
