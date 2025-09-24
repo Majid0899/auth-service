5c8fa736a46913271bf77ee71b28ce554e9ac0a9
Majid0899 <majidkhan0899@gmail.com>
2025-09-24 18:02:13 +0530

    feat(api): add user login controller

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/User.ts";
 import { ValidationError } from "sequelize";
-
+import {generateToken} from '../utils/jwt.ts'
 
 
 
@@ -12,7 +12,10 @@ interface RegisterUserBody {
   role?: "user" | "admin";
 }
 
-
+interface LoginUserBody {
+  email: string;
+  password: string;
+}
 
 const handleRegisterUser = async (
   req: Request<{}, {}, RegisterUserBody>,
@@ -57,8 +60,60 @@ const handleRegisterUser = async (
   }
 };
 
+const handleLoginUser = async (
+  req: Request<{}, {}, LoginUserBody>,
+  res: Response
+): Promise<Response>=> {
+  try {
+    const { email, password } = req.body;
+
+    //Find user including password
+    const user=await User.scope('withPassword').findOne({where:{email:email}})
+    if(!user) return res.status(404).json({success:false,error:"Invalid email Please enter valid email"})
+    
+    //compare password
+    const isMatch=await user.comparePassword(password)
+    if (!isMatch) return res.status(400).json({ message: "Invalid Password Please enter valid password" });
+
+    //payload for jwt
+    const payload={
+        id:user.id,
+        name:user.email,
+        role:user.role
+    }
+    
+    //return accessToken,refreshToken
+    const tokens=generateToken(payload)
+
+    return res.status(200).json({success:true,accessToken: tokens.accessToken,refreshToken: tokens.refreshToken,user:payload})
+
+
+  } catch (error) {
+
+    //Validation Error
+    if (error instanceof ValidationError) {
+      return res.status(400).json({
+        success: false,
+        errors: error.errors.map((e) => e.message),
+      });
+    }
+
+    //Server Error
+    if (error instanceof Error) {
+      return res
+        .status(500)
+        .json({ success: false, error: `Server Error ${error.message}` });
+    }
+
+    //Unknown Server Error
+    return res.status(500).json({
+      success: false,
+      error: "Unknown server error",
+    });
+    
+  }
+};
 
 
 
-
-export { handleRegisterUser};
+export { handleRegisterUser, handleLoginUser };
