c7fff9cc069185ba59288724890b0f9ac117a345
Majid0899 <majidkhan0899@gmail.com>
2025-09-25 16:14:17 +0530

    feat(api): add user logout controller

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
 import User from "../models/User.ts";
-import {  ValidationError } from "sequelize";
-import {generateToken, verifyrefreshToken,payload} from '../utils/jwt.ts'
-import {redisClient} from '../config/redis.ts'
-
-
+import { ValidationError } from "sequelize";
+import { generateToken, verifyrefreshToken, payload } from "../utils/jwt.ts";
+import { redisClient } from "../config/redis.ts";
 
 interface RegisterUserBody {
   name: string;
@@ -68,33 +66,48 @@ const handleRegisterUser = async (
 const handleLoginUser = async (
   req: Request<{}, {}, LoginUserBody>,
   res: Response
-): Promise<Response>=> {
+): Promise<Response> => {
   try {
     const { email, password } = req.body;
 
     //Find user including password
-    const user=await User.scope('withPassword').findOne({where:{email:email}})
-    if(!user) return res.status(404).json({success:false,error:"Invalid email Please enter valid email"})
-    
+    const user = await User.scope("withPassword").findOne({
+      where: { email: email },
+    });
+    if (!user)
+      return res
+        .status(404)
+        .json({
+          success: false,
+          error: "Invalid email Please enter valid email",
+        });
+
     //compare password
-    const isMatch=await user.comparePassword(password)
-    if (!isMatch) return res.status(400).json({ message: "Invalid Password Please enter valid password" });
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ message: "Invalid Password Please enter valid password" });
 
     //payload for jwt
-    const payload={
-        id:user.id,
-        name:user.email,
-        role:user.role
-    }
-    
+    const payload = {
+      id: user.id,
+      name: user.email,
+      role: user.role,
+    };
+
     //return accessToken,refreshToken
-    const tokens=generateToken(payload)
+    const tokens = generateToken(payload);
 
-    return res.status(200).json({success:true,accessToken: tokens.accessToken,refreshToken: tokens.refreshToken,user:payload})
-
-
+    return res
+      .status(200)
+      .json({
+        success: true,
+        accessToken: tokens.accessToken,
+        refreshToken: tokens.refreshToken,
+        user: payload,
+      });
   } catch (error) {
-
     //Validation Error
     if (error instanceof ValidationError) {
       return res.status(400).json({
@@ -115,32 +128,46 @@ const handleLoginUser = async (
       success: false,
       error: "Unknown server error",
     });
-    
   }
 };
 
-
-const hanldeRefreshToken=async(req:Request<{},{},RefreshTokenBody>,res:Response) :Promise<Response>=>{
-
+const hanldeRefreshToken = async (
+  req: Request<{}, {}, RefreshTokenBody>,
+  res: Response
+): Promise<Response> => {
   try {
-    const {refreshToken}=req.body;
-  if(!refreshToken) return res.status(401).json({success:false,error:"Refresh token required"})
-  
-  // check for blacklisted token
-     const isBlacklisted = await redisClient.get(refreshToken);
-    if (isBlacklisted) return res.status(403).json({success:false,error: "Token blacklisted" });
+    const { refreshToken } = req.body;
+    if (!refreshToken)
+      return res
+        .status(401)
+        .json({ success: false, error: "Refresh token required" });
 
-    const decode:payload=verifyrefreshToken(refreshToken)
-    const user=await User.findByPk(decode.id)
-    if(!user) return res.status(404).json({success:false,error:"User not found"})
+    // check for blacklisted token
+    const isBlacklisted = await redisClient.get(refreshToken);
+    if (isBlacklisted)
+      return res
+        .status(403)
+        .json({ success: false, error: "Token blacklisted" });
 
-    const tokens=generateToken({id:user.id,name:user.name,role:user.role})
+    const decode: payload = verifyrefreshToken(refreshToken);
+    const user = await User.findByPk(decode.id);
+    if (!user)
+      return res.status(404).json({ success: false, error: "User not found" });
 
-    return res.status(201).json({success:true,accessToken:tokens.accessToken,refreshToken:tokens.refreshToken})
+    const tokens = generateToken({
+      id: user.id,
+      name: user.name,
+      role: user.role,
+    });
 
-    
+    return res
+      .status(201)
+      .json({
+        success: true,
+        accessToken: tokens.accessToken,
+        refreshToken: tokens.refreshToken,
+      });
   } catch (error) {
-
     //Server Error
     if (error instanceof Error) {
       return res
@@ -154,9 +181,56 @@ const hanldeRefreshToken=async(req:Request<{},{},RefreshTokenBody>,res:Response)
       error: "Unknown server error",
     });
   }
-  
+};
 
-}
+const handleLogoutUser = async (
+  req: Request<{}, {}, RefreshTokenBody>,
+  res: Response
+) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken)
+      return res
+        .status(401)
+        .json({ success: false, error: "Refresh token required" });
 
+    
+    // Store in Redis blacklist until expiry
+    const decoded = verifyrefreshToken(refreshToken);
+    console.log(decoded)
 
-export { handleRegisterUser, handleLoginUser,hanldeRefreshToken };
+    if(!decoded.exp) return res.status(400).json({ success: false, error: "Invalid refresh token: no expiry" });
+    
+    // Calculate remaining lifetime (in seconds)
+    const expiry = decoded.exp - Math.floor(Date.now() / 1000);
+    
+
+    if(expiry<=0) return res.status(400).json({success:false,error:"Invalid refresh token: no expiry"})
+    
+      //Block the token
+    await redisClient.set(refreshToken, "blacklisted", { EX: expiry });
+
+    return res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    //Server Error
+    if (error instanceof Error) {
+      return res
+        .status(500)
+        .json({ success: false, error: `Server Error ${error.message}` });
+    }
+
+    //Unknown Server Error
+    return res.status(500).json({
+      success: false,
+      error: "Unknown server error",
+    });
+
+  }
+};
+
+export {
+  handleRegisterUser,
+  handleLoginUser,
+  hanldeRefreshToken,
+  handleLogoutUser,
+};
